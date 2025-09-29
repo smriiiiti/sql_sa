f4383ea167a921e32a214d2c92299a437ac9cb86
smriiiiti <smritiadhikari298@gmail.com>
2025-09-29 13:27:53 +0530

    Day 5 inner join boxoffice with movies table used order by and where condition

--- newTable.sql
+++ newTable.sql
@@ -1,3 +1,5 @@
 SELECT title, domestic_sales, international_sales from movies
 INNER JOIN Boxoffice
-on movies.id=boxoffice.movie_id;
+on movies.id=boxoffice.movie_id
+where international_sales>domestic_sales
+ORDER BY domestic_sales,international_sales ASC;
