3ef1e09ac792d7f61fb8891536d8aff41c4942a0
smriiiiti <smritiadhikari298@gmail.com>
2025-10-05 22:25:45 +0530

    Day 11 Order of execution

--- newTable.sql
+++ newTable.sql
@@ -1,2 +1,5 @@
-SELECT director, count(title) FROM movies
-group by director;
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
